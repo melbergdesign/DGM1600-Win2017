b416b0a604b4c52e9ed0c6446d618d70001f8ac7
Unknown <melbergdesign@gmail.com>
2017-02-06 10:08:00 -0700

    Moved more class work over

---
+++ Syntax.cs
@@ -0,0 +1,39 @@
+﻿//public = accessible to other classes
+//class and file have the same name
+//Class identifiers are Sentence casing; More than one word titles (classes) use CamelCase
+//Code blocks have {}; blocks of code have more than one line
+
+//C# is OOP
+//Classes are the cookie cutters
+
+public class Syntax
+{
+    //type of function (void = no return); identifier, same rules as classes; (arguments); {function contents}
+    //functions, loops, and conditionals all have ()
+    void ShootLaser()
+    {
+        print("hello world"); //each single line ends with a ;
+
+        if (2 + 2 == 4)
+        {
+            print("It's true");
+        }
+    }
+    //= means to assign value (assigning operator); == means to make a comparison (comparison operator)
+    //if statements require comparisons of some sort
+
+    
+    void IhaveMilk()
+    {
+        print("This is a glass of milk");
+    }
+   
+    
+    void EatACookie ()
+    {
+        if (cookie > 0)
+        {
+            print("I ate a cookie");
+        }
+    }
+}
