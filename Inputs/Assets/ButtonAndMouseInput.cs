149d07ffead71b1ffd7dc37ee36010f445944de9
Unknown <melbergdesign@gmail.com>
2017-02-17 10:48:37 -0700

    Inputs - No Hmwk
    Look for more opportunities for cool stuff

---
+++ Inputs/Assets/ButtonAndMouseInput.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonAndMouseInput : MonoBehaviour {
+
+	public void MyMouseClick(){
+		print ("I clicked the button.");
+	}
+	//Game Idea: Build a robot mobile game; kids are able to piece robot parts together.
+
+	//challenge: button click adds items to a list
+	//unity learn inputs & GUI tutorials
+}
---
+++ Inputs/Assets/KeyInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyInput : MonoBehaviour {
+
+	//2 kinds of inputs: Mouse and keyboard
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//inputs go here
+		//listen for a keyboard input
+
+		if(Input.GetKey(KeyCode.A)){//press the 'A' key
+			print("I hit A");
+		}
+		//get axis = directional pad on a gamepad controller
+		//location = Pokemon GO
+		/*else{
+		 * print ("No input");
+		 * }
+		 */
+	
+	}
+}
