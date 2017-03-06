5454434eff30aa4ce51c6934a9ac8ccc840a8636
Unknown <melbergdesign@gmail.com>
2017-03-06 11:00:00 -0700

    Scope and Access Modifiers Lecture

---
+++ Lectures/Assets/Scope & Access Modifiers/Access.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Access : MonoBehaviour {
+
+    public Scope home;
+
+	// Use this for initialization
+	void Start () {
+        print(home.owner);
+        //print(home.renter);
+        //print(home.Awakw());
+        home.Awake();
+	}
+	
+	
+}
---
+++ Lectures/Assets/Scope & Access Modifiers/Scope.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scope : MonoBehaviour {
+
+	public string owner = "Bob";//accessible in entire class
+    //variables can go in, they cannot come out{}
+    private string renter = "Kate";//unity can't see this, can be seen inside Awake
+
+	public void Awake(){
+		string cat = "Joe";//local variable, only available within the Awake function
+        if (cat == "Joe")
+        {
+            string mouse = "Helen";//Awake cannot see this variable, but the if statement can see cat variable
+            print(cat + " ate " + mouse);
+            print(renter);
+        }
+        //print(mouse); unable to print; does not exist outside if statement
+		print(cat);
+		print (owner);
+        print(renter);
+	}
+	//OOP = Awake is an object, anything inside is local only to awake.
+	void Start () {
+        string cat = "Frank";
+        print (cat);
+		print (owner);
+	}
+	
+
+}
