b4dac3129e6cc6d942859d280e3897bd5ef3badf
Unknown <melbergdesign@gmail.com>
2017-03-10 10:46:09 -0700

    Notes and Temp Convertor

---
+++ Lectures/Assets/VoidFunctions/FcConverter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FcConverter : MonoBehaviour {
+    //class that converts celsius to fahrenheit and back again
+    //connect functions to a button
+    //print converted value
+
+    public float temperature;
+
+	public void ConvertToCelsius()
+    {
+        float celsius;
+        celsius = (temperature - 32f) / 1.8f;
+        print(celsius);
+    }
+    public void ConvertToFahrenheit()
+    {
+        float fahrenheit;
+        fahrenheit = temperature * 1.8f + 32f;
+        print(fahrenheit);
+    }
+}
