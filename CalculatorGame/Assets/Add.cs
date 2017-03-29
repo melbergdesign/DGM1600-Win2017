3d1af7f4280c6f91eed5d37f72b6f77edac6a96b
Unknown <melbergdesign@gmail.com>
2017-03-29 11:08:19 -0600

    Started Calculator Project

---
+++ CalculatorGame/Assets/Add.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Add : MonoBehaviour {
+    public int num1 = 2;
+    public int num2 = 3;
+
+    public void OnClick()
+    {
+        //run the adding function
+    }
+
+	
+}
