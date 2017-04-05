45660a84b4bb6c55003ed9465abb30e191391cf5
Unknown <melbergdesign@gmail.com>
2017-04-05 11:11:53 -0600

    Calc Game - Parsing Works

--- CalculatorGame/Assets/Add.cs
+++ CalculatorGame/Assets/Add.cs
@@ -2,20 +2,19 @@
 using System.Collections;
 
 public class Add : MonoBehaviour {
-    public int entry1;
-    public int entry2;
+    public float entry1;
+    public float entry2;
 
     public void OnClick()
     {
-        print (entry1);
-        print (entry2);
+        print(AddNums(entry1, entry2));
         
     }
 
     //adding function
-    public int AddNums(int num1, int num2)
+    public float AddNums(float num1, float num2)
     {
-        int sum = num1 + num2;
+        float sum = num1 + num2;
         return sum;
     }
 
