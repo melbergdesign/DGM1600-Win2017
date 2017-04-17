526818042f5f4e62a01a713e7e1ac092f0444f15
Unknown <melbergdesign@gmail.com>
2017-04-17 11:11:35 -0600

    Math Operator Switch Works

---
+++ CalculatorGame/Assets/MathOperations.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MathOperations : MonoBehaviour {
+
+    /*public bool mathAdd = false;
+    public bool mathSub = false;
+    public bool mathMult = false;
+    public bool mathDiv = false;*/
+    public int mathFunction;
+
+    //public Operator myOperator;
+
+    //switch statement that assigns the math operator used
+    public void ButtonClick()
+    {
+
+        MathSelector();
+        /*if(mathAdd = true)
+        {
+            print("Add");
+        }
+        else if(mathSub = true)
+        {
+            print("sub");
+        }
+        else if (mathMult = true)
+        {
+            print("Mult");
+        }
+        else if(mathDiv = true)
+        {
+            print("Div");
+        }*/
+    }
+    public void MathSelector()
+    {
+        switch (mathFunction)//changes depending on operator button selected
+        {
+            case 1://add
+                print("add");
+                break;
+
+            case 2://subtract
+                print("subtract");
+                break;
+
+            case 3://multiply
+                print("multiply");
+                break;
+
+            case 4://divide
+                print("divide");
+                break;
+
+            default:
+                print("That is not a valid operation");
+                break;
+        }
+    }
+}
--- CalculatorGame/Assets/Number.cs
+++ CalculatorGame/Assets/Number.cs
@@ -4,15 +4,11 @@ using System.Collections;
 public class Number : MonoBehaviour {
 
 	public string numButton;
-	public TextEntry entry1 = new TextEntry();
-
+	
 	public void OnClick()
     {
         print(numButton);
-		//assign entry to text box; need classes
-
+		
     }
-	/*public float upTownFunc(){
-		numButton = entry1.TextConvert(numButton);
-	}*/
+	
 }
