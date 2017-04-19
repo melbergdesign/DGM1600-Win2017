0f2c8e3cc486d68a9091e5e32cf5632226b9fdaf
Mel Berg <melbergdesign@gmail.com>
2017-04-19 09:29:17 -0600

    Input Field Redo

---
+++ CalculatorGame/Assets/TextEntry.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextEntry : MonoBehaviour {
+
+    public InputField numberEntry;
+
+    public void PrintEntry()
+    {
+        print("This is a string " + numberEntry.text);
+    }
+
+	
+}
