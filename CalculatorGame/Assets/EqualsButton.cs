4f143f0e8c32069c6ff8a6fa9c848c4ba625acc8
Mel Berg <melbergdesign@gmail.com>
2017-04-30 21:30:48 -0600

    Equals button if statements

---
+++ CalculatorGame/Assets/EqualsButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EqualsButton : MonoBehaviour {
+    public bool add;
+    public bool subtract;
+    public bool multiply;
+    public bool divide;
+
+	public void OnClick()
+    {
+        if (add)
+        {
+            print("Add");
+            add = false;
+        }
+        if (subtract)
+        {
+            print("subtract");
+            subtract = false;
+        }
+        if (multiply)
+        {
+            print("multiply");
+            multiply = false;
+        }
+        if (divide)
+        {
+            print("divide");
+            divide = false;
+        }
+    }
+}
